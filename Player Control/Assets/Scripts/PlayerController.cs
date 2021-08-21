22522f823ac68126dd59aefce90b9c4755bea14f
pablozee <zeshanrasul@gmail.com>
2021-08-21 17:22:24 +0100

    Player movement based on Horizontal and Vertical Input

--- Player Control/Assets/Scripts/PlayerController.cs
+++ Player Control/Assets/Scripts/PlayerController.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    
+    private float speed = 20.0f;
+    private float turnSpeed = 72.0f;
+
+    private float horizontalInput;
+    private float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +19,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        forwardInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // Move the vehicle forward based on forward input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the vehicle based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
